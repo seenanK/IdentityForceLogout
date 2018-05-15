226475ebc09765893a7c8fda5e526e1ad997d2da
seenan <seenan@DOZ2015557D>
2018-05-15 09:53:50 +0400

    Add project files.

---
+++ IdentityForceLogout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityForceLogout.Startup))]
+namespace IdentityForceLogout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
